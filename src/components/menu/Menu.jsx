6889c5f833eec449e4eb17c930f0308b47c99126
Cansın <ayurekl97@gmail.com>
2022-12-14 22:23:40 +0300

    tap

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,17 +1,63 @@
 import React from "react";
 import "./Menu.css";
+import { useState } from "react";
 
 const Menu = () => {
+  const [vehicles, setVehicles] = useState([
+    {
+      id: 1,
+      name: "psdsd",
+      plate: "asdasd",
+      type: "adsasdsa",
+      gasoline: "asdasdsad",
+      location: "asdadads",
+    },
+    {
+      id: 1,
+      name: "psdsd",
+      plate: "asdasd",
+      type: "adsasdsa",
+      gasoline: "asdasdsad",
+      location: "asdadads",
+    },
+    {
+      id: 1,
+      name: "psdsd",
+      plate: "asdasd",
+      type: "adsasdsa",
+      gasoline: "asdasdsad",
+      location: "asdadads",
+    },
+  ]);
+  console.log(vehicles);
+  const [type, setType] = useState("");
   return (
     <>
-      <div className="menuContainer">
+      <div className="mainContainer">
         <div className="menuButtons">
-          <button className="buttons">ALL</button>
-          <button className="buttons">VEHICLES</button>
-          <button className="buttons">LOCATIONS</button>
+          <button onClick={() => setType("all")} className="buttons">
+            ALL
+          </button>
+          <button onClick={() => setType("vehicles")} className="buttons">
+            VEHICLES
+          </button>
+          <button onClick={() => setType("locations")} className="buttons">
+            LOCATIONS
+          </button>
+        </div>
+        <div className="menuContainer">
+          {type === "all" && (
+            <>
+              {vehicles.map((car) => {
+                <span>{car.gasoline}</span>;
+                <span>{car.id}</span>;
+              })}
+            </>
+          )}
+          {type === "vehicles" && <div>vehicles</div>}
+          {type === "locations" && <div>locations</div>}
         </div>
       </div>
-      <div className="menu"></div>
     </>
   );
 };
